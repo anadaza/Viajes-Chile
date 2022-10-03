2061abab074015099febfba8312fb32c9d72ba09
Daza Valenzuela Ana Sonia <adaza@hogardecristo.cl>
2022-10-03 01:03:57 -0300

    actualización js

---
+++ assets/js/estilos.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('[data-bs-taggle="tooltip"]').tooltip()
+})
+
+$("#enviar").click(function () {
+        alert("correo enviado");
+    });
+
+$(document).scroll(function(){
+    const y = $("html").scrolltop();
+
+    y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
+})
+
